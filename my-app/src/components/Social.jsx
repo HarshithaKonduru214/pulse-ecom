da74d3e1858596ce27234b82a6eb97f1f42e6667
HarshithaKonduru214 <harshitakonduru2244@gmail.com>
2022-03-24 00:21:29 +0530

    home page done

--- my-app/src/components/Social.jsx
+++ my-app/src/components/Social.jsx
@@ -1,11 +1,19 @@
-import { faTruck } from '@fortawesome/free-solid-svg-icons';
+import { faTruck, faRotateLeft, faCertificate, faTag } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "./style.css"
 
 const Social = ({ heading, content, icon }) => {
+    const iconHandler = (icon) => {
+        switch (icon) {
+            case "faTruck": return faTruck;
+            case "faRotateLeft" : return faRotateLeft;
+            case "faCertificate": return faCertificate;
+            default: return faTag;
+        }
+    }
     return (
         <div class="social-media-item flex-row p-2">
-            <div class="social-icons mt-2"><FontAwesomeIcon icon={ icon === "faTruck" && faTruck } /></div>
+            <div class="social-icons mt-2"><FontAwesomeIcon icon={ iconHandler(icon) } /></div>
             <div class="social-content flex-column p-2">
                 <div class="social-heading margin-bottom-xs">{ heading }</div>
                 <div class="social-desc mt-2 gray-text">{ content }</div>
--- my-app/src/components/SocialMedia.jsx
+++ my-app/src/components/SocialMedia.jsx
@@ -1,7 +1,5 @@
 import { Social } from "./Social"
 import "./style.css";
-import { faTruck } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const SocialMedia = () => {
     return (
@@ -9,22 +7,13 @@ const SocialMedia = () => {
             <div class="social-media flex-row justify-content-between m-2">
                 <div class="social-media-left flex-row justify-content-even gap"></div>
                     <Social heading="FREE SHIPPING" content="For orders above Rs.999." icon="faTruck" />
-                    <Social heading="EASY RETURNS" content="15 Day Easy Returns For Most Products." icon="faTruck" />
+                    <Social heading="EASY RETURNS" content="15 Day Easy Returns For Most Products." icon="faRotateLeft" />
 
-                    <Social heading="AUTHENTIC PRODUCTS" content="Sourced Directly from Brands/ Authorised Distributors." icon="faTruck" />
-                    <Social heading="10+ BRANDS" content="Well curated list of brands" icon="faTruck" />
+                    <Social heading="AUTHENTIC PRODUCTS" content="Sourced Directly from Brands/ Authorised Distributors." icon="faCertificate" />
+                    <Social heading="10+ BRANDS" content="Well curated list of brands" icon="faTag" />
                 <div/>
-                <div class="social-media-right">
-                <div class="social-media-item flex-column p-2 mt-2">
-                    <div class="social-heading align-self-center">SHOW US SOME LOVE</div>
-                    <div class="social-links flex-row align-self-center">
-                        <a href="#"><FontAwesomeIcon className="social-icons mt-2 mr-1" icon={ faTruck } /></a>
-                        <a href="#"><FontAwesomeIcon className="social-icons mt-2 mr-1" icon={ faTruck } /></a>                        
-                        <a href="#"><FontAwesomeIcon className="social-icons mt-2 mr-1" icon={ faTruck } /></a>
-                    </div>
-                </div>
             </div>
-        </div></div>
+        </div>
         
     )
 }
