ac7ab028f11e65ea4e4794667537806f6f455821
HarshithaKonduru214 <harshitakonduru2244@gmail.com>
2022-03-24 00:30:15 +0530

    Create index.js

---
+++ my-app/src/components/index.js
@@ -0,0 +1,9 @@
+export { Banner } from "./Banner";
+export { Footer } from "./Footer";
+export { Mockman } from "./Mockman";
+export { Nav } from "./Nav";
+export { Review } from "./review";
+export { ShopBy  } from "./ShopBy";
+export { Social } from "./Social";
+export { SocialMedia } from "./SocialMedia";
+
